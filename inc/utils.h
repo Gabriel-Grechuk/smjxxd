b01735a2c94165684d6b9b1df1a63a228a067fc3
Gabe <gabrielgrechuk@outlook.com>
2025-05-25 22:32:02 -0300

    #5 - Add utils

---
+++ inc/utils.h
@@ -0,0 +1,9 @@
+#ifndef _SMJXXD_UTILS_H_
+#define _SMJXXD_UTILS_H_
+
+#include <genesis.h>
+
+// Decrease and prevent negative numbers.
+u16 smjxxd_utils_drain(u16 value, u16 decrease);
+
+#endif
---
+++ src/utils.c
@@ -0,0 +1,10 @@
+#include "utils.h"
+
+// Decrease and prevent negative numbers.
+u16 smjxxd_utils_drain(u16 value, u16 decrease)
+{
+  s16 res = value - decrease;
+  if (res < 0)
+    return 0;
+  return res;
+}
