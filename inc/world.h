8f4520a872e5fb57d67ea650fa35f754a14d2c1a
Gabe <gabrielgrechuk@outlook.com>
2025-05-26 13:41:16 -0300

    #5 - Update waves data and implement mobs spawn

--- inc/world.h
+++ inc/world.h
@@ -8,15 +8,97 @@
  * positioning, bullets and hits/clips.
  */
 
-#define MAX_MOBS 79
-#define MAX_MONSTERS 20
+#define MONSTERS_AMOUNT 10
+#define MONSTERS_ON_SCREEN 6
+#define MONSTER_TYPE_COUNT 6
 
-static u16 mob_count = 0; 
-GameObject mobs_list[MAX_MOBS];
+typedef struct {
+  MobType type;
+  u8 amount;
+} WaveMonsters;
+
+// 7 waves 0 - 6;
+static u8 wave = 0;
+static u8 mob_count = 0;
+static GameObject monster_list[MONSTERS_AMOUNT];
 
 /**
- *  Updates  the world elements.
+ * Monsters per level.
+ *
+ * Hard codded for now. Should be a probabilistic function.
+ *
+ * NOTE: keep all zombies until the other mobs have sprites.
+ */
+
+static WaveMonsters waves[7][6] = {
+    {
+        {.type = ZOMBIE, .amount = 10},
+        {.type = SKELETON, .amount = 0},
+        {.type = MUTANT, .amount = 0},
+        {.type = GHOST, .amount = 0},
+        {.type = IMP, .amount = 0},
+        {.type = BOSS, .amount = 0},
+    },
+    {
+        {.type = ZOMBIE, .amount = 10},
+        {.type = SKELETON, .amount = 0},
+        {.type = MUTANT, .amount = 0},
+        {.type = GHOST, .amount = 0},
+        {.type = IMP, .amount = 0},
+        {.type = BOSS, .amount = 0},
+    },
+    {
+        {.type = ZOMBIE, .amount = 10},
+        {.type = SKELETON, .amount = 0},
+        {.type = MUTANT, .amount = 0},
+        {.type = GHOST, .amount = 0},
+        {.type = IMP, .amount = 0},
+        {.type = BOSS, .amount = 0},
+    },
+
+    {
+        {.type = ZOMBIE, .amount = 10},
+        {.type = SKELETON, .amount = 0},
+        {.type = MUTANT, .amount = 0},
+        {.type = GHOST, .amount = 0},
+        {.type = IMP, .amount = 0},
+        {.type = BOSS, .amount = 0},
+    },
+    {
+        {.type = ZOMBIE, .amount = 10},
+        {.type = SKELETON, .amount = 0},
+        {.type = MUTANT, .amount = 0},
+        {.type = GHOST, .amount = 0},
+        {.type = IMP, .amount = 0},
+        {.type = BOSS, .amount = 0},
+    },
+    {
+        {.type = ZOMBIE, .amount = 10},
+        {.type = SKELETON, .amount = 0},
+        {.type = MUTANT, .amount = 0},
+        {.type = GHOST, .amount = 0},
+        {.type = IMP, .amount = 0},
+        {.type = BOSS, .amount = 0},
+    },
+
+    {
+        {.type = ZOMBIE, .amount = 10},
+        {.type = SKELETON, .amount = 0},
+        {.type = MUTANT, .amount = 0},
+        {.type = GHOST, .amount = 0},
+        {.type = IMP, .amount = 0},
+        {.type = BOSS, .amount = 0},
+    },
+};
+
+/**
+ * Updates the world elements.
  */
 void smjxxd_world_update();
+
+/**
+ * Spawn the monsters in the world.
+ */
+void smjxxd_world_spawn_monsters();
 
 #endif
--- src/world.c
+++ src/world.c
@@ -1,6 +1,25 @@
 #include "world.h"
 
-void smjxxd_world_update()
-{
+#include "mob.h"
+
+/**
+ * Local implementations.
+ */
+void smjxxd_world_spawn(WaveMonsters *wave);
+
+/**
+ * Module implementations.
+ */
+void smjxxd_world_update() {
   // TODO: Implement.
+}
+
+void smjxxd_world_spawn_monsters() { smjxxd_world_spawn(waves[wave]); }
+
+inline void smjxxd_world_spawn(WaveMonsters *wave) {
+  u8 monster_list_seek = 0;
+  for (u8 i = 0; i < MONSTERS_AMOUNT; i++)
+    for (u8 amount = 0; amount < wave[i].amount; amount++) {
+      smjxxd_mob_init(&monster_list[monster_list_seek], wave[i].type);
+    }
 }
