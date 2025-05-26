55f13a6f6eef266b8a6b960954b7e318e167bb75
Gabe <gabrielgrechuk@outlook.com>
2025-05-25 21:44:18 -0300

    #5 - Background setup

---
+++ inc/background.h
@@ -0,0 +1,11 @@
+#ifndef _SMJXXD_BACKGROUND_H_
+#define _SMJXXD_BACKGROUND_H_
+
+#include <genesis.h>
+#include "globals.h"
+#include "game_object.h"
+#include "resources.h"
+
+u16 smjxxd_background_init(u16 ind);
+
+#endif
