f198c8148f0eac5614146078acc2f49f3453905f
Gabe <gabrielgrechuk@outlook.com>
2025-05-25 21:44:30 -0300

    #5 - Update main()

--- src/main.c
+++ src/main.c
@@ -1,9 +1,46 @@
 #include <genesis.h>
+#include <sprite_eng.h>
 
-int main(bool hardReset) {
-  VDP_drawText("Hey There!", 12, 12);
+#include "globals.h"
 
-  while (TRUE) {
+#include "game_object.h"
+#include "utils.h"
+#include "background.h"
+
+u16 ind = TILE_USER_INDEX;
+
+void game_init()
+{
+  VDP_setScreenWidth320();
+  SPR_init();
+
+  ind += smjxxd_background_init(ind);
+}
+
+static inline void game_update()
+{
+  // TODO: implement.
+}
+
+int main(bool resetType)
+{
+  // Soft reset doesn't clear RAM. Can lead to bugs.
+  if (!resetType)
+  {
+    SYS_hardReset();
+  }
+  SYS_showFrameLoad(true);
+  game_init();
+
+  SYS_doVBlankProcess();
+
+  kprintf("Free RAM after Game Init: %d", MEM_getFree());
+
+  while (true)
+  {
+    game_update();
+
+    SPR_update();
     SYS_doVBlankProcess();
   }
 
